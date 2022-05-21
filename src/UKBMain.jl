1b4800acdc29029272505e9d7eafcee474b5a0f0
Olivier Labayle <olabayle@gmail.com>
2022-05-20 22:29:00 -0700

    add decode function

--- src/UKBMain.jl
+++ src/UKBMain.jl
@@ -1,5 +1,77 @@
 module UKBMain
+using CSV
+using DataFrames
 
-# Write your package code here.
+"""
+    field_metadata(fields::DataFrame, field_id::Int)
 
+Retrieves the row corresponding to `field_id` from the metadata Dataframe `fields`.
+"""
+function field_metadata(fields::DataFrame, field_id::Int)
+    row_id = findfirst(x -> x.field_id == field_id, eachrow(fields))
+    return fields[row_id, :]
+end
+
+"""
+    first_instance_fields(datasetfile)
+
+Retrieves fields that correspond to first instances only.
+"""
+function first_instance_fields(datasetfile)
+    all_fields = names(CSV.read(datasetfile, DataFrame, limit=1))
+    final_fields = String[] 
+    for field in all_fields[2:end]
+        if split(field, "-")[2][1] == '0'
+            push!(final_fields, field)
+        end
+    end
+    return vcat(all_fields[1], final_fields)
+end
+
+convertcolumn(column::Vector{T}, ::Type{T}) where T = column
+
+function convertcolumn(column::Vector{Union{Missing, T₁}}, ::Type{T₂}) where {T₁, T₂}
+    convert(Vector{Union{T₂, Missing}}, column)
+end
+
+function convertcolumn(column::Vector{T₁}, ::Type{T₂}) where {T₁, T₂}
+    convert(Vector{T₂}, column)
+end
+
+function process!(dataset, colname, field_metadata)
+    column = dataset[:, colname]
+    if field_metadata.value_type == 11
+        dataset[!, colname] = convertcolumn(column, Int)
+    elseif field_metadata.value_type == 21
+        dataset[!, colname] = convertcolumn(column, Bool)
+    end
+end
+
+"""
+    decode(parsed_args)
+
+A main dataset is decoded:
+    - only the first instance of each field is kept
+    - datatypes are converted as per field_value. Currently this only supports fields 
+    that represent Integers and Booleans.
+"""
+function decode(parsed_args)
+    datasetfile = parsed_args["dataset"]
+    fieldsfile = parsed_args["fields"]
+    outfile = parsed_args["out"]
+
+    columns = first_instance_fields(datasetfile)
+    dataset = CSV.read(datasetfile, DataFrame, select=columns)
+
+    fields = CSV.read(fieldsfile, DataFrame)
+    
+    for colname in names(dataset)[2:end]
+        field_id = parse(Int, split(colname, "-")[1])
+        process!(dataset, colname, field_metadata(fields, field_id))
+    end
+
+    CSV.write(outfile, dataset)
+end
+
+export decode
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,84 @@
 using UKBMain
 using Test
+using CSV
+using DataFrames
 
-@testset "UKBMain.jl" begin
-    # Write your tests here.
+const DATASETFILE = joinpath("data", "data.csv")
+const FIELDSFILE = joinpath("data", "field.txt")
+
+@testset "Test first_instance_fields" begin
+    columns = UKBMain.first_instance_fields(DATASETFILE)
+    @test columns == ["eid", "21003-0.0", "22001-0.0"]
+end
+
+@testset "Test field_metadata" begin
+    fields_meta = CSV.read(FIELDSFILE, DataFrame)
+
+    age_meta = UKBMain.field_metadata(fields_meta, 21003)
+    @test age_meta.field_id == 21003
+    @test age_meta.value_type == 11
+    sex_meta = UKBMain.field_metadata(fields_meta, 22001)
+    @test sex_meta.field_id == 22001
+    @test sex_meta.value_type == 21 
+end
+
+@testset "Test process!" begin
+    fields_meta = CSV.read(FIELDSFILE, DataFrame)
+    dataset = DataFrame(Dict(
+        "21003-0.0" => [64., 32., 23, 11],
+        "21003-1.0" => [64, 32, 23, 11],
+        "21003-2.0" => [64., 32, missing, 11],
+        "22001-0.0" => [1, 0, 1, missing],
+        "22001-1.0" => [1, 0, 1, 0],
+        "22001-2.0" => [1, 0, 1, 1.]
+        ))
+
+    age_meta = UKBMain.field_metadata(fields_meta, 21003)
+    sex_meta = UKBMain.field_metadata(fields_meta, 22001)
+    # First column
+    @test eltype(dataset[!, "21003-0.0"]) === Float64
+    UKBMain.process!(dataset, "21003-0.0", age_meta)
+    @test eltype(dataset[!, "21003-0.0"]) === Int64
+
+    # Second column
+    @test eltype(dataset[!, "21003-1.0"]) === Int
+    UKBMain.process!(dataset, "21003-1.0", age_meta)
+    @test eltype(dataset[!, "21003-1.0"]) === Int64
+
+    # Third column
+    @test eltype(dataset[!, "21003-2.0"]) === Union{Float64, Missing}
+    UKBMain.process!(dataset, "21003-2.0", age_meta)
+    @test eltype(dataset[!, "21003-2.0"]) === Union{Int64, Missing}
+
+    # Fourth column
+    @test eltype(dataset[!, "22001-0.0"]) === Union{Int, Missing}
+    UKBMain.process!(dataset, "22001-0.0", sex_meta)
+    @test eltype(dataset[!, "22001-0.0"]) === Union{Bool, Missing}
+
+    # Fifth column
+    @test eltype(dataset[!, "22001-1.0"]) === Int
+    UKBMain.process!(dataset, "22001-1.0", sex_meta)
+    @test eltype(dataset[!, "22001-1.0"]) === Bool
+
+    # Sixth column
+    @test eltype(dataset[!, "22001-2.0"]) === Float64
+    UKBMain.process!(dataset, "22001-2.0", sex_meta)
+    @test eltype(dataset[!, "22001-2.0"]) === Bool
+
+end
+
+@testset "Test decode" begin
+    parsed_args = Dict(
+        "dataset" => DATASETFILE,
+        "fields" => FIELDSFILE,
+        "out" => "processed_output.csv"
+    )
+    decode(parsed_args)
+    output = CSV.read(parsed_args["out"], DataFrame)
+    @test names(output) == ["eid", "21003-0.0", "22001-0.0"]
+    @test output[:, "eid"] isa Vector{Int}
+    @test output[:, "21003-0.0"] isa Vector{Int}
+    @test output[:, "22001-0.0"] isa Vector{Union{Bool, Missing}}
+
+    rm(parsed_args["out"])
 end
